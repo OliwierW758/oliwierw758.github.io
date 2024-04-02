47abdac1d17509da4fe5a146947c710101286bf4
Shadow75857 <106625978+Shadow75857@users.noreply.github.com>
2024-04-02 21:08:29 +0200

    sorting #1

--- projekt/content.js
+++ projekt/content.js
@@ -1,71 +1,56 @@
-var data = 
+var data_file = 
 `
 {
     "articles": [
         {
             "image": "img/kot_europejski.png",
             "title": "Kot europejski krótkowłosy",
-            "description": "Naturalna, stosunkowo niedawno uznana rasa kota. Uznanie rasy nastąpiło ze względu na konieczność ochrony pierwotnych genów kotów występujących od wieków na terenie Europy."
+            "description": "Naturalna, stosunkowo niedawno uznana rasa kota. Uznanie rasy nastąpiło ze względu na konieczność ochrony pierwotnych genów kotów występujących od wieków na terenie Europy.",
+            "category": "abdef"
         },
         {
             "image": "img/kot_perski.png",
             "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
+            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku.",
+            "category": "abdef"
         },
         {
             "image": "img/kot_brytyjski.png",
             "title": "Kot brytyjski",
-            "description": "Rasa kota, której początki wywodzą od kotów sprowadzonych na Wyspy Brytyjskie przez legiony rzymskie, wykorzystujące je do walki ze szczurami. Legendy tej nie udało się jednak udowodnić. Faktem jest, że rasa wywodzi się od kotów domowych zamieszkujących Wielką Brytanię (nie jest to jednak domowy kot \\"brytyjski\\"). Na skutek II wojny światowej istnienie tej rasy zostało zagrożone. W latach 50. XX wieku hodowcy, przy pomocy krzyżowania z persem niebieskim, wzmocnili populację i poprawili cechy rasy."
+            "description": "Rasa kota, której początki wywodzą od kotów sprowadzonych na Wyspy Brytyjskie przez legiony rzymskie, wykorzystujące je do walki ze szczurami. Legendy tej nie udało się jednak udowodnić. Faktem jest, że rasa wywodzi się od kotów domowych zamieszkujących Wielką Brytanię (nie jest to jednak domowy kot \\"brytyjski\\"). Na skutek II wojny światowej istnienie tej rasy zostało zagrożone. W latach 50. XX wieku hodowcy, przy pomocy krzyżowania z persem niebieskim, wzmocnili populację i poprawili cechy rasy.",
+            "category": "abdef"
         },
         {
             "image": "img/maine_coon.png",
             "title": "Maine coon",
-            "description": "Rasa kota pochodząca z Ameryki Północnej. Koty rasy maine coon wyróżnia wielkość (są jednymi z największych kotów domowych), inteligencja i żywiołowość."
+            "description": "Rasa kota pochodząca z Ameryki Północnej. Koty rasy maine coon wyróżnia wielkość (są jednymi z największych kotów domowych), inteligencja i żywiołowość.",
+            "category": "abdef"
         },
 
 
-
-
-        
         {
-            "image": "img/kot_perski.png",
-            "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
+            "image": "img/owczarek_niemiecki.png",
+            "title": "Owczarek niemiecki",
+            "description": "Jedna z ras psów należąca do grupy psów pasterskich sklasyfikowana do sekcji psów pasterskich (owczarskich). Według klasyfikacji FCI podlega próbom pracy. Zaliczany do psów obronnych. Typ wilkowaty.",
+            "category": "abdef"
         },
         {
-            "image": "img/kot_perski.png",
-            "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
+            "image": "img/labrador_retriever.png",
+            "title": "Labrador retriever",
+            "description": "Rasa psów należąca do grupy psów aportujących, płochaczy i psów wodnych, zaklasyfikowana do sekcji psów aportujących. Typ wyżłowaty. Podlega próbom pracy.",
+            "category": "abdef"
         },
         {
-            "image": "img/kot_perski.png",
-            "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
+            "image": "img/yorkshire_terrier.png",
+            "title": "Yorkshire terrier",
+            "description": "Rasa psa należąca do grupy terierów, zaklasyfikowana do sekcji terierów miniaturowych.",
+            "category": "abdef"
         },
         {
-            "image": "img/kot_perski.png",
-            "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
-        },
-        {
-            "image": "img/kot_perski.png",
-            "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
-        },
-        {
-            "image": "img/kot_perski.png",
-            "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
-        },
-        {
-            "image": "img/kot_perski.png",
-            "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
-        },
-        {
-            "image": "img/kot_perski.png",
-            "title": "Kot perski",
-            "description": "Stara rasa kota, zaliczana do grupy długowłosych. W Europie pojawiła się w XVII wieku."
+            "image": "img/buldog_francuski.png",
+            "title": "Buldog francuski",
+            "description": "Jedna z ras psów, należąca do grupy psów do towarzystwa, zaklasyfikowana do sekcji małych psów molosowatych. Typ dogowaty. Nie podlega próbom pracy.",
+            "category": "abdef"
         }
     ]
 } 
--- projekt/script.js
+++ projekt/script.js
@@ -2,11 +2,27 @@ function add_article(img, title, text) {
     document.getElementById("art-list").innerHTML += `<li><img src = "${img}"><h3 class = "title">${title}</h3><p>${text}</p></li>`;
 }
 
+function reset() {
+    document.getElementById("art-list").innerHTML = "";
+}
+
 function reload_articles() {
 
-    let content = JSON.parse(data);
+    reset();
 
-    for(const article of content.articles) {
-        add_article(article.image, article.title, article.description)
+    let selected_category = document.querySelector('input[name="option"]:checked').value;
+
+    console.log(selected_category);
+
+    let data = JSON.parse(data_file);
+
+    for(let article of data.articles) {
+        if(article.category = selected_category) {
+
+            console.log(article.title);
+            console.log(article.category);
+            
+            add_article(article.image, article.title, article.description);
+        }
     }
 }
